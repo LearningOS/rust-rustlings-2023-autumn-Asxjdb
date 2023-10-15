32db74eecef5682794f11216d01dca3907b17bcd
szl <2812689534@qq.com>
2023-10-15 23:36:47 +0800

    update

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -7,7 +7,7 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
+
 
 fn string_slice(arg: &str) {
     println!("{}", arg);
@@ -16,7 +16,7 @@ fn string(arg: String) {
     println!("{}", arg);
 }
 
-// fn main() {
+fn main() {
     string_slice("blue");
     string("red".to_string());
     string(String::from("hi"));
@@ -28,15 +28,4 @@ fn string(arg: String) {
     string("Happy Monday!".to_string().replace("Mon", "Tues"));
     string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
-fn main() {
-    string_slice("blue");
-    string("red".to_string());
-    string(String::from("hi"));
-    string_slice("rust is fun!".to_owned());
-    string_slice("nice weather".into());
-    string_slice(format!("Interpolation {}", "Station"));
-    string_slice(&String::from("abc")[0..1]);
-    string_slice("  hello there ".trim());
-    string("Happy Monday!".to_string().replace("Mon", "Tues"));
-    string_slice("mY sHiFt KeY iS sTiCkY".to_lowercase());
-}
+
