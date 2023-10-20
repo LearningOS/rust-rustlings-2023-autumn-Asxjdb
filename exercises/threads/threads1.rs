408900950d251aa5cbdcbecb1078cf79bdfd80a2
szl <2812689534@qq.com>
2023-10-20 19:15:29 +0800

    update

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -8,7 +8,6 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::thread;
 use std::time::{Duration, Instant};
@@ -23,10 +22,12 @@ fn main() {
             start.elapsed().as_millis()
         }));
     }
+    // handles.iter().for_each(|handle|{handle.join();});
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
+        results.push(handle.join().unwrap())
     }
 
     if results.len() != 10 {
